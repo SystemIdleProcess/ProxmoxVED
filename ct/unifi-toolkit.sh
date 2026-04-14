c696e1bb37658858977037743e7c493d9183c16a
SystemIdleProcess <SystemIdleProcess@users.noreply.github.com>
2026-04-14 16:11:28 -0400

    feat: add unifi-toolkit container

---
+++ ct/unifi-toolkit.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVED/main/misc/build.func)
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: SystemIdleProcess
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source: https://github.com/Crosstalk-Solutions/unifi-toolkit
+
+APP="UI Toolkit"
+var_tags="${var_tags:-notification}"
+var_cpu="${var_cpu:-2}"
+var_ram="${var_ram:-2048}"
+var_disk="${var_disk:-2}"
+var_os="${var_os:-debian}"
+var_version="${var_version:-13}"
+var_unprivileged="${var_unprivileged:-1}"
+
+header_info "$APP"
+variables
+color
+catch_errors
+
+function update_script() {
+  header_info
+  check_container_storage
+  check_container_resources
+
+  if [[ ! -d "/opt/unifi-toolkit" ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  if check_for_gh_release "unifi-toolkit" "Crosstalk-Solutions/unifi-toolkit"; then
+    msg_info "Stopping Service"
+    systemctl stop unifi-toolkit
+    msg_ok "Stopped Service"
+
+    setup_python "3.12"
+    cd /opt/unifi-toolkit
+    source venv/bin/activate
+    fetch_and_deploy_gh_release "unifi-toolkit" "Crosstalk-Solutions/unifi-toolkit" "tarball"
+    msg_info "Updating Unifi-Toolkit"
+    pip install -r requirements.txt --upgrade
+    alembic upgrade head
+    msg_ok "Updated Unifi-Toolkit"
+
+    msg_info "Starting Service"
+    systemctl start unifi-toolkit
+    msg_ok "Started Service"
+    msg_ok "Updated successfully!"
+  fi
+  exit
+}
+
+start
+build_container
+description
+
+msg_ok "Completed successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:8000${CL}"
---
+++ install/unifi-toolkit-install.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+# Copyright (c) 2021-2026 community-scripts ORG
+# Author: SystemIdleProcess
+# License: MIT | https://github.com/community-scripts/ProxmoxVED/raw/main/LICENSE
+# Source: https://github.com/Crosstalk-Solutions/unifi-toolkit
+
+source /dev/stdin <<<"$FUNCTIONS_FILE_PATH"
+color
+verb_ip6
+catch_errors
+setting_up_container
+network_check
+update_os
+
+msg_warn "WARNING: This script will run an external installer from a third-party source (https://www.kasmweb.com/)."
+msg_warn "The following code is NOT maintained or audited by our repository."
+msg_warn "If you have any doubts or concerns, please review the installer code before proceeding:"
+msg_custom "${TAB3}${GATEWAY}${BGN}${CL}" "\e[1;34m" "→  https://raw.githubusercontent.com/Crosstalk-Solutions/unifi-toolkit/refs/heads/main/setup.sh"
+echo
+read -r -p "${TAB3}Do you want to continue? [y/N]: " CONFIRM
+if [[ ! "$CONFIRM" =~ ^([yY][eE][sS]|[yY])$ ]]; then
+  msg_error "Aborted by user. No changes have been made."
+  exit 10
+fi
+
+setup_python "3.12"
+fetch_and_deploy_gh_release "unifi-toolkit" "Crosstalk-Solutions/unifi-toolkit" "tarball"
+
+msg_info "Setup Unifi-Toolkit"
+cd /opt/unifi-toolkit
+$STD bash setup.sh
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+msg_ok "Setup Unifi-Toolkit"
+
+msg_info "Creating Service"
+cat <<EOF >/etc/systemd/system/unifi-toolkit.service
+[Unit]
+Description=Unifi-Toolkit Service
+After=network-online.target
+
+[Service]
+Type=simple
+WorkingDirectory=/opt/unifi-toolkit
+ExecStart=/opt/unifi-toolkit/run.py
+Restart=always
+RestartSec=30
+
+[Install]
+WantedBy=multi-user.target
+EOF
+systemctl enable -q --now unifi-toolkit
+msg_ok "Created Service"
+
+motd_ssh
+customize
+cleanup_lxc
